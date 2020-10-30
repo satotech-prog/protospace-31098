04f761d5116614726f54a688e1cd2fc2b3f5994a
satotech-prog <72504810+satotech-prog@users.noreply.github.com>
2020-10-30 14:49:11 +0900

    修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -31,9 +31,9 @@ class PrototypesController < ApplicationController
   end
 
   def update
-       @prototypes = Prototype.find(params[:id])
-    if @prototypes.update(prototype_params)
-      redirect_to root_path
+       @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
     else
       render :edit
     end
